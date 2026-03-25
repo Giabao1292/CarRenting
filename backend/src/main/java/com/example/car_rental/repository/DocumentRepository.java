73a42ffc7ef579226dceb739e3df82f777dbcfac
luyt661 <luytpv1703@gmail.com>
2026-03-25 20:10:22 +0700

    update

--- backend/src/main/java/com/example/car_rental/repository/DocumentRepository.java
+++ backend/src/main/java/com/example/car_rental/repository/DocumentRepository.java
@@ -12,6 +12,8 @@ import java.util.Optional;
 
 public interface DocumentRepository extends JpaRepository<Document, Integer> {
 
+    Optional<Document> findTopByUserIdAndDocTypeOrderByCreatedAtDesc(Integer userId, String docType);
+
     @EntityGraph(attributePaths = {"user"})
     @Query("""
         SELECT d
